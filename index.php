56132d28c4add77b74801a8d0287de1f98664eba
Yves Charvis <Yves Charvis>
2017-03-07 14:10:30 +0100

    exo4

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>exo 4</title>
+</head>
+<body>
+<?php include "variable.php" ?>
+</body>
+</html>
---
+++ variable.php
@@ -0,0 +1,10 @@
+<?php
+
+$string="string";
+$int= 5;
+$float= 1.1;
+$bolean= true;
+
+echo "$string", "$int","$float", "$bolean" ;
+
+?>
