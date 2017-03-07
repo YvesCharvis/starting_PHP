a88f9746fe85719ab8bd7658a5bf2af1f2e9194f
Yves Charvis <Yves Charvis>
2017-03-07 14:29:28 +0100

    exo6

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html>
 <link rel="stylesheet" type="text/css" href="style/css/style.css">
 <head>
-	<title>master</title>
+	<title>exo 6</title>
 </head>
 <body>
-
+<?php require "variable.php" ?>
 </body>
 </html>
--- variable.php
+++ variable.php
@@ -1,3 +1,5 @@
 <?php
+$nom= "Yves";
 
+echo "<p>Bonjour,</p>".$nom."<p>Comment va tu ?</p>";
 ?>
