b317d79496ac3d4dd55c5044b3b9ec26813f974d
Yves Charvis <Yves Charvis>
2017-03-07 14:48:56 +0100

    exo 7

--- variable.php
+++ variable.php
@@ -1,3 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" type="text/css" href="style/css/style.css">
+<head>
+	<title>EXO 7</title>
+</head>
+<body>
 
+<?php
+$nom= "Yves";
+$prenom= "Nicolas";
+$age = 58;
+
+echo "Bonjour Mister".$nom. "".$prenom.".<br>Tu as:".$age. "";
 ?>
+</body>
+</html>
+
