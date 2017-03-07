627c77b3dd01592586d9cd52f336c1f4171f858c
Yves Charvis <Yves Charvis>
2017-03-07 16:34:26 +0100

    exo2.2

---
+++ condition.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>exo2.2</title>
+</head>
+<body>
+<?php
+$iseasy= true;
+
+if ($iseasy = true) {
+	echo "good";
+}else{
+	echo "c'est difficile";
+}
+?>
+</body>
+</html>
