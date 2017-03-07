071b6ea70c909bfd285a36ded1ad709532793905
Yves Charvis <Yves Charvis>
2017-03-07 14:53:59 +0100

    exo8

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <link rel="stylesheet" type="text/css" href="style/css/style.css">
 <head>
-	<title>master</title>
+	<title>exo 8</title>
 </head>
 <body>
 <a href="variable.php">EXERCICE 1</a>
--- variable.php
+++ variable.php
@@ -1,3 +1,17 @@
-<?php
 
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" type="text/css" href="style/css/style.css">
+<head>
+	<title>exo 8</title>
+</head>
+<body>
+<?php 
+$addition= 3+4;
+$multipl= 5*20;
+$division= 45/5;
+
+echo "Le resultat est:".$addition.'<br>'.$multipl.'<br>'.$division."";
 ?>
+</body>
+</html>
