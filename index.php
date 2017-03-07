1126799e8d228d8270d25a9c60556aa9fcd826d2
Yves Charvis <Yves Charvis>
2017-03-07 13:53:29 +0100

    exo2

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>exo 2</title>
+</head>
+<body>
+<?php include ("variable.php") ?>
+</body>
+</html>
---
+++ variable.php
@@ -0,0 +1,12 @@
+<?php
+
+$donner= "Yves";
+$prenom = "nicolas";
+$nom = "charvis";
+$age = 20;
+
+echo "$donner";
+echo "$prenom", "$nom", "$age";
+
+
+?>
