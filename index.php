db306b926525f0e128533cbaa6e278c169915401
Yves Charvis <Yves Charvis>
2017-03-07 13:59:25 +0100

    exo 3

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>exo 3</title>
+</head>
+<body>
+<?php include "variable.php" ?>
+</body>
+</html>
---
+++ variable.php
@@ -0,0 +1,9 @@
+<?php
+
+$km = 1;
+echo "$km";
+
+echo "$km" + 2; 
+echo "$km" + 124;
+
+?>
