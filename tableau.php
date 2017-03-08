9fa0a92a2f1fc7ecec64414c574f39ea9ee0701b
Yves Charvis <Yves Charvis>
2017-03-08 14:21:26 +0100

    exo5.10

--- tableau.php
+++ tableau.php
@@ -1,3 +1,21 @@
 <?php
 
+//crée un tableau assiociatif  
+$arrayName = array(
+
+					02 => "Aisne" ,
+					59 => "Nord" ,
+					60=> "Oise",
+					62 =>"Pas-de-Calais",
+					80 =>"Somme",
+
+				);
+
+
+ 
+ foreach ($arrayName as $key => $value) {
+  	
+  	echo "Le département".$value. "a le numero".$key."<br>";
+ 
+ }
 ?>
