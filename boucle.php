f767eadd771ea3b08309e5ddf9e971e416ab673a
Yves Charvis <Yves Charvis>
2017-03-08 09:39:58 +0100

    exo3.0

---
+++ boucle.php
@@ -0,0 +1,8 @@
+<?php
+$var= 0;
+
+while ($var <= 10){
+	echo "$var";
+	$var++;
+}
+?>
--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 <body>
 <a href="variable.php">EXERCICE 1</a>
 <a href="condition.php">EXERCICE 2</a>
+<a href="boucle.php">EXERCICE 3</a>
 </body>
 </html>
