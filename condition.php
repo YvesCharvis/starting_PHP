192395fbf59fbfad44032502bde747bac49a87a8
Yves Charvis <Yves Charvis>
2017-03-08 09:19:27 +0100

    exo2.3

---
+++ condition.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>exo2.3</title>
+</head>
+<body>
+<?php
+$age= 18;
+$genre=['homme','femme'];
+
+if ($age>= 18) {
+	if ($genre= 'homme') {
+		echo "Vous etes majeur et un homme";
+}else($age<18) {
+	if ($genre='femme') {
+			echo "Vous une putain de femme mineur!";
+	}
+} 
+		
+
+}
+
+
+?>
+</body>
+</html>
