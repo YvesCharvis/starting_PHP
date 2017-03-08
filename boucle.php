c715bfd39698c78a0b1e3c3a733679a59ca61ebb
Yves Charvis <Yves Charvis>
2017-03-08 09:58:33 +0100

    exo3.1

--- boucle.php
+++ boucle.php
@@ -1,4 +1,17 @@
 <?php
+$a = 0;
+$b = 40;
+
+while ($a <= $b) {
+   $a++;
+   
+}
+// 2 variable une a 0 lautre eentre 1 et 100
+ // faire une boucle tanque le variable 1 n'est pas superieur a 20
+//faire le calcul 
+//afficher le resultat
+//incrementer var 1
+
 
 
 ?>
