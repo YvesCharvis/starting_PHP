af1f22063fc4aea4f1078c99fd2a0142a2d929d1
Yves Charvis <Yves Charvis>
2017-03-09 09:25:22 +0100

    masterfor

--- parametre.php
+++ parametre.php
@@ -5,6 +5,8 @@
 </head>
 <body>
 <a href="parametre/execice1.php?nom=Nemare&prenom=Jean);">EXERCICE 1</a>
+<a href="parametre/exercice4.php?langage=PHP&serveur=LAMP">EXERCICE 4</a>
+<a href="parametre/exercice6.php?batiment=<script>alert(‘lol’)</script>&salle=101">EXECICE6.1</a>
 
 </body>
 </html>
